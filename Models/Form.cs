cb4772e9b3fceea8529de4a2879fa6fb1a8c913f
maude <maude.antoin@gmail.com>
2024-07-12 11:37:00 +0200

    commit

---
+++ Models/Form.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class Form
+    {
+        [Required]
+        public string Nom {  get; set; }
+        [Required]
+        public string Prenom { get; set; }
+        [Required]
+        public string Genre { get; set; }
+        [Required]
+        public string Adresse { get; set; }
+        [RegularExpression(@"^([\w]+)@([\w]+)\.( [\w]+)$")]
+        [StringLength(5, MinimumLength = 5)]
+        [Required]
+        public string Cp { get; set; }
+        [Required]
+        public string Ville { get; set; }
+        //[RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Required]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [Required]
+        public DateTime Date { get; set; }
+    }
+}
